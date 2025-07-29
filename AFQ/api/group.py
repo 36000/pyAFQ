1224adcee39953842893de418bebcafc894ffce3
36000 <jk6.28@outlook.com>
2025-07-29 16:59:24 -0700

    BFs

--- AFQ/api/group.py
+++ AFQ/api/group.py
@@ -1125,6 +1125,7 @@ class ParallelGroupAFQ():
                 pAFQ_kwargs.dwi_data_file,
                 pAFQ_kwargs.bval_file,
                 pAFQ_kwargs.bvec_file,
+                pAFQ_kwargs.t1_file,
                 pAFQ_kwargs.results_dir,
                 **pAFQ_kwargs.kwargs)
             pAFQ.export(attr_name)
@@ -1174,6 +1175,7 @@ class ParallelGroupAFQ():
                 pAFQ_kwargs.dwi_data_file,
                 pAFQ_kwargs.bval_file,
                 pAFQ_kwargs.bvec_file,
+                pAFQ_kwargs.t1_file,
                 pAFQ_kwargs.results_dir,
                 **pAFQ_kwargs.kwargs)
             pAFQ.export_all(viz, xforms, indiv)
